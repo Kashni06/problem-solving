2c5ffd493a088e0ee35f280bb4784129920885c3
Kashni <138650336+Kashni06@users.noreply.github.com>
2025-09-14 12:34:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Three way partitioning/three-way-partitioning.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    // Function to partition the array around the range such
+    // that array is divided into three parts.
+    void threeWayPartition(vector<int>& arr, int a, int b) {
+        // code here
+        int n=arr.size();
+        int low=0,mid=0,high=n-1;
+        while(mid<=high){
+            if(arr[mid]<a){
+                swap(arr[low],arr[mid]);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]>b){
+                swap(arr[mid],arr[high]);
+             
+                high--;
+            }
+            else{
+                mid++;
+            }
+        }
+    }
+};
