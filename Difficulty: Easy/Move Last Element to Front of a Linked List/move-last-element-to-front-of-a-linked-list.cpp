5659e3ccc30c96b0a8eddc26a717b153a88e3d74
Kashni <138650336+Kashni06@users.noreply.github.com>
2025-10-07 16:14:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move Last Element to Front of a Linked List/move-last-element-to-front-of-a-linked-list.cpp
@@ -0,0 +1,18 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    Node *moveToFront(Node *head) {
+        // code here
+        Node* curr=head;
+        Node* prev=NULL;
+        while(curr->next!=NULL){
+            prev=curr;
+            curr=curr->next;
+        }
+        curr->next=head;
+        prev->next=NULL;
+        head=curr;
+        return head;
+    }
+};
