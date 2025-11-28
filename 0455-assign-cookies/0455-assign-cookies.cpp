0cfe36bd284efb7448a8632de6feb39129e730b2
Kashni <138650336+Kashni06@users.noreply.github.com>
2025-11-28 12:33:48 +0530

    Time: 8 ms (61.78%), Space: 44.8 MB (74.47%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+        //TWO POINTERS APPROACH -> firslty sort both arrays greed factor and cookie size
+        // then using two pointer one on geed factor and other for cookie size if it satisfy the condition then move ahead both pointers meand child will be satified and count will be increase if not only j will be increment and then return i 
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
 
