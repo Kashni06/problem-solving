7f88627bb41eb3744c51f4103e2ef98e22b8f11a
Kashni <138650336+Kashni06@users.noreply.github.com>
2025-11-29 19:46:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.15%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int start = 0, end = x, ans = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid == 0) {   // Prevent division by zero
+                start = mid + 1;
+                continue;
+            }
+
+            if (mid == x / mid) {
+                return mid;
+            }
+            else if (mid < x / mid) {
+                ans = mid;
+                start = mid + 1;
+            } 
+            else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
