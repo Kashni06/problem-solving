c544f9b7fe254bc2c8e78f6cf328b26607f7c41a
Kashni <138650336+Kashni06@users.noreply.github.com>
2026-01-08 20:29:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int evenlyDivides(int n) {
+        int count = 0;
+        int original = n;
+
+        while (n > 0) {
+            int digit = n % 10;
+            n = n / 10;
+
+            if (digit != 0 && original % digit == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
