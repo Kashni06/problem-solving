04d970b54b65fb80f6152de102b42143adaf4057
Kashni <138650336+Kashni06@users.noreply.github.com>
2026-01-15 20:20:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Pattern/print-pattern.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void helper(int n, vector<int>& ans) {
+        ans.push_back(n);
+
+        if (n <= 0) return;
+
+        helper(n - 5, ans);
+        ans.push_back(n);  // backtracking
+    }
+
+    vector<int> pattern(int n) {
+        vector<int> ans;
+        helper(n, ans);
+        return ans;
+    }
+};
