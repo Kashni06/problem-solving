9b3ac7cfd553dc885805001f02681e7166bc42f2
Kashni <138650336+Kashni06@users.noreply.github.com>
2026-01-26 18:37:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even and Odd/even-and-odd.cpp
@@ -0,0 +1,23 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    void reArrange(int arr[], int N) {
+        // code here
+        int i=0;
+        int j=1;
+        while(i<N && j<N){
+            if(arr[i]%2!=0 && arr[j]%2==0){
+                swap(arr[i],arr[j]);
+                i+=2;
+                j+=2;
+            }
+            else if(arr[i]%2!=0){
+                j+=2;
+            }
+            else{
+                i+=2;
+            }
+        }
+    }
+};
